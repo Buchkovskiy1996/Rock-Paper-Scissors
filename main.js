b8b6866164c7762faad2eef4412fd5737230c21b
Buchkovskiy1996 <buchkovskiybogdan28061996@gmail.com>
2023-01-18 16:11:46 +0300

    edited js functional add html file css

--- main.js
+++ main.js
@@ -1,35 +1,56 @@
 "use strict"
 
+const btnRock = document.querySelector(".btn__rock")
+const btnPaper = document.querySelector(".btn__paper")
+const btnScissors = document.querySelector(".btn__scissors")
+const mainWinner = document.querySelector(".main__winner")
+const btnInner = document.querySelectorAll(".btn__inner")
+const scoreWinPlayer = 0;
+const scoreWinComp = 0;
+const round = document.querySelectorAll(".round");
+
 function getComputerChoice() {
-    let choice = ["Камень", "Бумага", "Ножицы"]; // масив слов
+    let choice = ["Камень", "Бумага", "Ножницы"]; // масив слов
     let randomRPS = choice[Math.floor(Math.random() * choice.length)]; // генерация слов 
     return randomRPS;
   }
 
-
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection == "Камень" && computerSelection == "Бумага") {
-        return "Вы проиграли! Бумага выиграла проти каменя!"
-    } else if (playerSelection == "Бумага" && computerSelection == "Камень") {
-        return "Вы выиграли! Бумага выиграла проти каменя!"
-    } else if(playerSelection == "Камень" && computerSelection == "Ножицы") {
-        return "Вы выиграли! Камень выиграл проти ножниц!"
-    } else if(playerSelection == "Ножицы" && computerSelection == "Камень") {
-        return "Вы проиграли! Камень выиграл проти ножицы!"
-    } else if(playerSelection == "Бумага" && computerSelection == "Ножицы") {
-        return "Вы проиграли! Ножицы выиграли проти бумаги!"
-    } else if(playerSelection == "Ножицы" && computerSelection == "Бумага") {
-        return "Вы выиграли! Ножицы выиграли проти бумаги!"
-    } else if(playerSelection == computerSelection) {
-        return "А у нас ничья!"
-    }
-}
-
-function game() {
-    for(let i = 0; i<2;i++) {
-        let playerSelectionc = prompt("А давай сыиграем в игру камень, ножицы ,бумага?");
+function game () {
+    btnRock.addEventListener("click", function(){      
         let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelectionc,computerSelection))
-    }
+        if(btnRock == btnRock && computerSelection == "Бумага"){
+            mainWinner.textContent = "Вы проиграли! Бумага победила  бумагу, и робот заработал 1 очко!"
+        } else if(btnRock == btnRock && computerSelection == "Ножницы") {
+            mainWinner.textContent = "Вы выиграли! Камень победил ножницы , вы заработали 1 очко";
+            scoreWinPlayer++
+        } else if (btnRock == computerSelection) {
+            console.log("А у нас ничья!")
+            mainWinner.textContent = "А у нас ничья!"
+        }
+    })
+
+    btnPaper.addEventListener("click", function(){
+        let computerSelection = getComputerChoice();
+        if(btnPaper == btnPaper && computerSelection == "Ножницы") {
+            mainWinner.textContent = "Вы проиграли! Ножницы победили  бумагу, и робот заработал 1 очко!"
+        } else if(btnPaper == btnPaper && computerSelection == "Камень") {
+            mainWinner.textContent = "Вы выиграли! Бумага победил каменя , вы заработали 1 очко";
+        } else if(btnPaper == btnPaper && computerSelection == "Бумага") {
+            mainWinner.textContent = "А у нас ничья!"
+        }
+    
+    })
+    
+    btnScissors.addEventListener("click", function(){
+        let computerSelection = getComputerChoice();
+        if(btnScissors == btnScissors && computerSelection == "Бумага") {
+            mainWinner.textContent = "Вы выиграли! Ножницы победила бумагу , вы заработали 1 очко";
+        }  else if(btnScissors == btnScissors && computerSelection == "Камень") {
+            mainWinner.textContent = "Вы проиграли! Камень победил  ножницы, и робот заработал 1 очко!"
+        } else if(btnPaper == btnScissors && computerSelection == "Камень") {
+            mainWinner.textContent = "А у нас ничья!"
+        }
+    })
 }
-game();
+
+game()
